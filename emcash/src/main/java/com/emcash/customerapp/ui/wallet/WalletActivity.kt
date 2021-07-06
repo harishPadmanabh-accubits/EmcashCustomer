2930540490f66ca335ae0d5b36c9013d44a95c7d
harishpadmanabh-HP <harish.p@accubits.com>
2021-07-06 23:56:06 +0530

    add wallet screen

---
+++ emcash/src/main/java/com/emcash/customerapp/ui/wallet/WalletActivity.kt
@@ -0,0 +1,12 @@
+package com.emcash.customerapp.ui.wallet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.emcash.customerapp.R
+
+class WalletActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wallet2)
+    }
+}
