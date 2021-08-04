c574d89bc8798ced5903181bfa90089a4535d001
harishpadmanabh <harishpadmanabh@gmail.com>
2021-08-04 09:01:08 +0530

    bug fixes 1.0.2

--- emcash/src/main/java/com/emcash/customerapp/ui/newPayment/PaymentChatFragment.kt
+++ emcash/src/main/java/com/emcash/customerapp/ui/newPayment/PaymentChatFragment.kt
@@ -5,6 +5,8 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.emcash.customerapp.R
+import com.emcash.customerapp.extensions.openActivity
+import com.emcash.customerapp.ui.home.HomeActivity
 import kotlinx.android.synthetic.main.item_chat.*
 import kotlinx.android.synthetic.main.payment_chats.*
 
@@ -16,6 +18,10 @@ class PaymentChatFragment:Fragment(R.layout.payment_chats) {
         super.onViewCreated(view, savedInstanceState)
         cl_chats.setOnClickListener {
             viewModel.gotoScreen(NewPaymentScreens.RECEIPT)
+        }
+
+        iv_back.setOnClickListener {
+            requireActivity().onBackPressed()
         }
     }
 
